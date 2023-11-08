3f6db39e62ce9415bf608bdb6f612d9633c0d77e
Diprama laptop <50134297+diprama@users.noreply.github.com>
2023-11-08 08:05:55 +0700

    Update login.php

--- login.php
+++ login.php
@@ -100,7 +100,7 @@ session_destroy();
                                 <form class="form-signin" role="form" action="login_validasi.php" method="POST" name="form1" target="_self" id="form1">
                                     <div class="mb-1">
                                         <label for="login-email" class="form-label">Username</label>
-                                        <input type="text" class="form-control" id="txtUser" name="txtUser" placeholder="emailkamu@mail.com" aria-describedby="login-email" tabindex="1" autofocus />
+                                        <input type="text" class="form-control" id="txtUser" name="txtUser" placeholder="username nya kaka" aria-describedby="login-email" tabindex="1" autofocus />
                                     </div>
 
                                     <div class="mb-1">
