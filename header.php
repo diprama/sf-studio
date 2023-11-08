5296a8628ddef3c197187194b4aa90266d21112d
Diprama laptop <50134297+diprama@users.noreply.github.com>
2023-11-08 08:14:43 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <!-- BEGIN: Head-->
-
+<?php
+include_once "library/inc.seslogin.php";
+?>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
