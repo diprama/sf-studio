8682a34f4c7460e5dd2321b17558165cfd180b0a
diprama <50134297+diprama@users.noreply.github.com>
2023-12-04 05:53:04 +0700

    get

--- js/chaindropdown/config.js
+++ js/chaindropdown/config.js
@@ -4,17 +4,17 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 
 
 	
-	$("#partnumber").change(function(){ // Ketika user mengganti atau memilih data kategori
+	$("#jenisfoto").change(function(){ // Ketika user mengganti atau memilih data kategori
 		// $("#subkategori").hide(); // Sembunyikan dulu combobox subkategori nya
 		// // $("#loading").show(); // Tampilkan loadingnya
 		// $("#subkategoriform").show(); // munculkan box subkategori
-		$('#subkategori').attr('disabled', false);
+		$('#paket').attr('disabled', false);
 		
 	
 		$.ajax({
 			type: "POST", // Method pengiriman data bisa dengan GET atau POST
 			url: "product_subpart.php", // Isi dengan url/path file php yang dituju
-			data: {kategori : $("#partnumber").val()}, // data yang akan dikirim ke file yang dituju
+			data: {kategori : $("#jenisfoto").val()}, // data yang akan dikirim ke file yang dituju
 			dataType: "json",
 			beforeSend: function(e) {
 				if(e && e.overrideMimeType) {
@@ -26,7 +26,7 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 
 				// set isi dari combobox subkategori
 				// lalu munculkan kembali combobox subkategorinya
-				$("#subkategori").html(response.data_subkategori).show();
+				$("#paket").html(response.data_subkategori).show();
 			},
 			error: function (xhr, ajaxOptions, thrownError) { // Ketika ada error
 				alert(thrownError); // Munculkan alert error
