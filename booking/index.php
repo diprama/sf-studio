a0744543835d5c578b455a5b1958cf007cc240c3
diprama <50134297+diprama@users.noreply.github.com>
2023-12-04 05:58:52 +0700

    Update index.php

--- booking/index.php
+++ booking/index.php
@@ -74,6 +74,8 @@ date_default_timezone_set("Asia/Jakarta");
   <!-- timepicker style -->
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Chaindrop -->
+  <script src="js/chaindropdown/config.js" type="text/javascript"></script>
 
 </head>
 
@@ -246,8 +248,6 @@ date_default_timezone_set("Asia/Jakarta");
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
   <!-- timepicker -->
   <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
-  <!-- Chaindrop -->
-  <script src="js/chaindropdown/config.js" type="text/javascript"></script>
 
 </body>
 
