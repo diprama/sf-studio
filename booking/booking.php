bc10eeb9ee973182930d0625cd439b05ae2e2c66
diprama <50134297+diprama@users.noreply.github.com>
2023-12-04 07:40:56 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -1,12 +1,5 @@
 <?php
-ob_start();
-session_start();
-ini_set("max_execution_time", 0);
-ini_set('display_errors', 1);
 include_once "library/inc.connection.php";
-// include_once "library/inc.library.php";
-date_default_timezone_set("Asia/Jakarta");
-// include "buka_file.php";
 ?>
 
 <!-- Copyright @ 2018 PT. Rentas Media Indonesia (www.rentas.co.id) -->
