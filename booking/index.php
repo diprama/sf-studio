08852acfe0eb5ab1cfa7625ee75b0f0d282e59da
diprama <50134297+diprama@users.noreply.github.com>
2023-12-04 07:02:14 +0700

    Update index.php

--- booking/index.php
+++ booking/index.php
@@ -132,7 +132,7 @@ date_default_timezone_set("Asia/Jakarta");
                         <form id="login_form" method="POST">
                           <div class="row spacing3">
 
-                            <div class="col-sm-12">
+                            <div class="input-group mb-3">
                               <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
                               <div class="input-group-append">
                                 <span class="input-group-text" id="basic-addon2">@example.com</span>
