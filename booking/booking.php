1378a5c3c364ab7420c7ccee8c9df5c29b218a72
diprama <50134297+diprama@users.noreply.github.com>
2023-12-08 23:37:45 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -65,8 +65,6 @@ setlocale(LC_TIME, 'id_ID');
   <link href="./assets/css/styles.css" rel="stylesheet">
   <!-- font-awesome -->
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <!-- datepicker styles -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.min.css">
   <!-- timepicker style -->
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
