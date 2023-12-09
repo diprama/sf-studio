e69e5167887e42a811a7a86a281794f798ff5e19
diprama <50134297+diprama@users.noreply.github.com>
2023-12-09 19:08:36 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -345,52 +345,7 @@ setlocale(LC_TIME, 'id_ID');
         }
       });
 
-      $(function() {
-        var now = new Date();
-        var openingTime = new Date(now);
-        var closingTime = new Date(now);
-
-
-
-        openingTime.setHours(8, 0, 0); // Jam buka pukul 08:00
-        closingTime.setHours(20, 0, 0); // Jam tutup pukul 16:00
-
-        // Initialize the timepicker with a 20-minute interval
-        $('#timepicker').timepicker({
-          timeFormat: 'H:i',
-          interval: 20,
-          step: 20,
-          dynamic: false,
-          dropdown: true,
-          scrollbar: true,
-          minTime: openingTime,
-          maxTime: closingTime,
-          disableTimeRanges: getDisabledRanges()
-        });
-
-        function getDisabledRanges() {
-          var disabledRanges = [];
-          var currentTime = new Date();
-
-          if (currentTime < openingTime) {
-            // Jika sekarang sebelum jam buka, nonaktifkan waktu sampai jam buka
-            disabledRanges.push(['12:00am', formatTime(openingTime)]);
-          } else if (currentTime >= closingTime) {
-            // Jika sekarang setelah jam tutup, nonaktifkan semua waktu
-            disabledRanges.push(['12:00am', '11:59pm']);
-          } else {
-            // Jika sekarang di antara jam buka dan jam tutup, nonaktifkan waktu sampai sekarang
-            disabledRanges.push(['12:00am', formatTime(currentTime)]);
-          }
-
-          return disabledRanges;
-        }
-
-        function formatTime(date) {
-          return date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-        }
-      });
-
+  
 
 
     });
