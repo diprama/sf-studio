0b5bc079db8ac7fa56ea8a74a3ceaf36551d8600
diprama <50134297+diprama@users.noreply.github.com>
2023-12-09 17:34:03 +0700

    121

--- booking/get_background.php
+++ booking/get_background.php
@@ -11,29 +11,29 @@ $jenis   = $Channel[0];
 
 // defaul isi validasi
 $validasi = 'NO';
-$html2 = "";
+$html = "";
 // validasi apakah mempunya sub part atau tidak
-$sql2 = mysqli_query($koneksidb, "SELECT * FROM master_background where jenis='Photo Session' ORDER BY jenis asc");
+$sql = mysqli_query($koneksidb, "SELECT * FROM master_background  ORDER BY jenis asc");
 // $cekQry = mysqli_query($koneksidb, $sql) or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
-if (mysqli_num_rows($sql2) >= 1) {
-	$validasi2 = 'YES';
+if (mysqli_num_rows($sql) >= 1) {
+	$validasi = 'YES';
 }
 
 
 // end validasi
-if ($validasi2 != 'YES') {
-	$html2 .= "Jenis Foto belum ditentukan."; // Tambahkan tag option ke variabel $htm
+if ($validasi != 'YES') {
+	$html .= "Jenis Foto belum ditentukan."; // Tambahkan tag option ke variabel $htm
 } else {
-	while ($data2 = mysqli_fetch_array($sql2)) { // Ambil semua data dari hasil eksekusi $sql
-		$html2 .= "<option value='" . $data2['background'] . "'>" . $data2['background'] . "    </option>"; // Tambahkan tag option ke variabel $html
+	while ($data = mysqli_fetch_array($sql)) { // Ambil semua data dari hasil eksekusi $sql
+		$html .= "<option value='" . $data['paket'] . "'>" . $data['paket'] . "    </option>"; // Tambahkan tag option ke variabel $html
 	}
 }
 // // Buat query untuk menampilkan data sub part dengan part number alias tertentu(sesuai yang dipilih user pada form)
 // $sql = mysqli_query($koneksidb, "SELECT * FROM master_material where product_id='$part_number' ORDER BY product_id");
 
-$callback2 = array('data_subkategori2' => $html2); // Masukan variabel html tadi ke dalam array $callback dengan index array : data_kota
+$callback = array('data_subkategori' => $html); // Masukan variabel html tadi ke dalam array $callback dengan index array : data_kota
 
-echo json_encode($callback2); // konversi varibael $callback menjadi JSON
+echo json_encode($callback); // konversi varibael $callback menjadi JSON
 
 
 // Buat variabel untuk menampung tag-tag option nya
--- booking/js3/chaindropdown/config.js
+++ booking/js3/chaindropdown/config.js
@@ -4,17 +4,17 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 
 
 	
-	$("#paket").change(function(){ // Ketika user mengganti atau memilih data kategori
+	$("#jenisfoto").change(function(){ // Ketika user mengganti atau memilih data kategori
 		// $("#subkategori").hide(); // Sembunyikan dulu combobox subkategori nya
 		// // $("#loading").show(); // Tampilkan loadingnya
 		// $("#subkategoriform").show(); // munculkan box subkategori
-		$('#background').attr('disabled', false);
+		$('#paket').attr('disabled', false);
 		
 	
 		$.ajax({
 			type: "POST", // Method pengiriman data bisa dengan GET atau POST
-			url: "get_background.php", // Isi dengan url/path file php yang dituju
-			data: {kategori : $("#paket").val()}, // data yang akan dikirim ke file yang dituju
+			url: "get_pilihan.php", // Isi dengan url/path file php yang dituju
+			data: {kategori : $("#jenisfoto").val()}, // data yang akan dikirim ke file yang dituju
 			dataType: "json",
 			beforeSend: function(e) {
 				if(e && e.overrideMimeType) {
@@ -26,7 +26,7 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 
 				// set isi dari combobox subkategori
 				// lalu munculkan kembali combobox subkategorinya
-				$("#background").html(response.data_subkategori).show();
+				$("#paket").html(response.data_subkategori).show();
 			},
 			error: function (xhr, ajaxOptions, thrownError) { // Ketika ada error
 				alert(thrownError); // Munculkan alert error
