b5760d6f1beee9966700bd3cd9e5bedfd68620c0
diprama <50134297+diprama@users.noreply.github.com>
2023-12-09 16:41:25 +0700

    Update master_jadwal.php

--- admin/master_jadwal.php
+++ admin/master_jadwal.php
@@ -77,12 +77,12 @@ $jam_tutup = $myData['jam_tutup'];
                 <div class="row g-1">
                     <div class="col-md-4 col-12 mb-3 position-relative">
                         <label class="form-label" for="validationTooltip01">Jam Buka</label>
-                        <input type="text" id="timepicker" class="form-control" placeholder="Jam Buka" value="<?= $jam_buka ?>" required />
+                        <input type="time"  class="form-control" placeholder="Jam Buka" value="<?= $jam_buka ?>" required />
                         <div class="valid-tooltip">Looks good!</div>
                     </div>
                     <div class="col-md-4 col-12 mb-3 position-relative">
                         <label class="form-label" for="validationTooltip02">Jam Tutup</label>
-                        <input type="text" id="timepicker" class="form-control" placeholder="Jam Tutup" value="<?= $jam_tutup ?>" required />
+                        <input type="time"  class="form-control" placeholder="Jam Tutup" value="<?= $jam_tutup ?>" required />
                         <div class="valid-tooltip">Looks good!</div>
                     </div>
                 </div>
