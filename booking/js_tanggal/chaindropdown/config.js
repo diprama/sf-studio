0d209b6c62c33d8fde2258468ef4e48c724d6c4f
diprama <50134297+diprama@users.noreply.github.com>
2023-12-09 19:36:45 +0700

    Update config.js

--- booking/js_tanggal/chaindropdown/config.js
+++ booking/js_tanggal/chaindropdown/config.js
@@ -2,26 +2,25 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 	// Kita sembunyikan dulu untuk loadingnya
 	$("#loading").hide();
 
+	var date_input = document.getElementById('datepicker');
 
-	
-	$("#datepicker").change(function(){ // Ketika user mengganti atau memilih data kategori
-		// $("#subkategori").hide(); // Sembunyikan dulu combobox subkategori nya
-		// // $("#loading").show(); // Tampilkan loadingnya
-		// $("#subkategoriform").show(); // munculkan box subkategori
+	date_input.onchange = function () {
+		console.log(this.value);
+
 		$('#waktu').attr('disabled', false);
-		
-	
+
+
 		$.ajax({
 			type: "POST", // Method pengiriman data bisa dengan GET atau POST
 			url: "get_datepicker.php", // Isi dengan url/path file php yang dituju
-			data: { kategori: $("#datepicker").val()}, // data yang akan dikirim ke file yang dituju
+			data: { kategori: $("#datepicker").val() }, // data yang akan dikirim ke file yang dituju
 			dataType: "json",
-			beforeSend: function(e) {
-				if(e && e.overrideMimeType) {
+			beforeSend: function (e) {
+				if (e && e.overrideMimeType) {
 					e.overrideMimeType("application/json;charset=UTF-8");
 				}
 			},
-			success: function(response){ // Ketika proses pengiriman berhasil
+			success: function (response) { // Ketika proses pengiriman berhasil
 				// $("#loading").hide(); // Sembunyikan loadingnya
 
 				// set isi dari combobox subkategori
@@ -32,6 +31,13 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 				alert(thrownError); // Munculkan alert error
 			}
 		});
+	}
+	
+	$("#datepicker").change(function(){ // Ketika user mengganti atau memilih data kategori
+		// $("#subkategori").hide(); // Sembunyikan dulu combobox subkategori nya
+		// // $("#loading").show(); // Tampilkan loadingnya
+		// $("#subkategoriform").show(); // munculkan box subkategori
+
 	});
 	
 });
