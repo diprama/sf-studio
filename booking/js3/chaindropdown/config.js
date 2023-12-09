11697727a4d1ba8a03fbc583d34da3c4ef8587d8
diprama <50134297+diprama@users.noreply.github.com>
2023-12-09 17:09:53 +0700

    1

--- booking/js3/chaindropdown/config.js
+++ booking/js3/chaindropdown/config.js
@@ -26,7 +26,7 @@ $(document).ready(function(){ // Ketika halaman sudah siap (sudah selesai di loa
 
 				// set isi dari combobox subkategori
 				// lalu munculkan kembali combobox subkategorinya
-				$("#background").html(response.data_subkategori).show();
+				$("#background").html(response.data_subkategori1).show();
 			},
 			error: function (xhr, ajaxOptions, thrownError) { // Ketika ada error
 				alert(thrownError); // Munculkan alert error
