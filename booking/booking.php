96e40711bf3d057a46cdb1a0596a0070323e79f3
diprama <50134297+diprama@users.noreply.github.com>
2023-12-10 20:56:48 +0700

    Update booking.php

--- booking/booking.php
+++ booking/booking.php
@@ -169,8 +169,8 @@ if (isset($_POST['btnSubmit'])) {
                                       # code...
                                       // panggil database
                                       echo "SELECT * from jadwal j where j.status ='0' and j.availability ='0' and j.jam not in (select jam from booking where tanggal = '$txtTanggal'";
-                                      
-                                      $mySql  = "SELECT * from jadwal j where j.status ='0' and j.availability ='0' and j.jam not in (select jam from booking where tanggal = '$txtTanggal') order by j.jam asc;";
+
+                                      $mySql  = "SELECT * from jadwal j where j.status ='0' and j.availability ='0' order by j.jam asc;";
                                       $myQry  = mysqli_query($koneksidb, $mySql)  or die("RENTAS ERP ERROR : " . mysqli_error($koneksidb));
                                       while ($myData = mysqli_fetch_array($myQry)) {
                                         $jamsekarang = date("H:i", strtotime("+60 minutes"));
