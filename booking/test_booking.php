ffd276f1e76dde125eb1bbfa7425d90f93e67bfc
diprama <50134297+diprama@users.noreply.github.com>
2023-12-29 23:40:36 +0700

    Update test_booking.php

--- booking/test_booking.php
+++ booking/test_booking.php
@@ -378,64 +378,57 @@ if (isset($_POST['btnSubmit'])) {
                                 <h3 id="monthHeader"></h3>
                                 <p id="yearHeader"></p>
                               </div>
-                              <!-- <form id="calendarForm" method="post" action="/submit-date"> -->
-                              <table class="table-calendar" id="">
+                              <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" name="form2" target="_self">
+                                <table class="table-calendar" id="">
 
-                                <thead id="thead-month"></thead>
+                                  <thead id="thead-month"></thead>
 
-                                <tbody id="calendar-body"></tbody>
+                                  <tbody id="calendar-body"></tbody>
 
-                              </table>
-
-
-
-                              <div class="footer-container-calendar">
-
-                                <label for="month">Jump To: </label>
-
-                                <select id="month">
-
-                                  <option value=0>Jan</option>
-
-                                  <option value=1>Feb</option>
-
-                                  <option value=2>Mar</option>
-
-                                  <option value=3>Apr</option>
-
-                                  <option value=4>May</option>
-
-                                  <option value=5>Jun</option>
-
-                                  <option value=6>Jul</option>
-
-                                  <option value=7>Aug</option>
-
-                                  <option value=8>Sep</option>
-
-                                  <option value=9>Oct</option>
-
-                                  <option value=10>Nov</option>
-
-                                  <option value=11>Dec</option>
-
-                                </select>
-
-                                <select id="year"></select>
-
-                              </div>
+                                </table>
 
 
 
-                              <form id="calendarForm" method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-                                <div>
-                                  <span id="date-picked"></span>
+                                <div class="footer-container-calendar">
+
+                                  <label for="month">Jump To: </label>
+
+                                  <select id="month">
+
+                                    <option value=0>Jan</option>
+
+                                    <option value=1>Feb</option>
+
+                                    <option value=2>Mar</option>
+
+                                    <option value=3>Apr</option>
+
+                                    <option value=4>May</option>
+
+                                    <option value=5>Jun</option>
+
+                                    <option value=6>Jul</option>
+
+                                    <option value=7>Aug</option>
+
+                                    <option value=8>Sep</option>
+
+                                    <option value=9>Oct</option>
+
+                                    <option value=10>Nov</option>
+
+                                    <option value=11>Dec</option>
+
+                                  </select>
+
+                                  <select id="year"></select>
+
                                 </div>
 
-                                
-                                <!-- Your existing calendar code here -->
 
 
+                                <div id="date-picked"></div>
+                                <input type="hidden" id="selectedDate" name="selectedDate">
                             </div>
 
                           </div>
@@ -459,164 +452,7 @@ if (isset($_POST['btnSubmit'])) {
       </div>
     </div>
   </div><!-- Scripts--><!-- Put the 3rd/plugins javascript here-->
-
-  <script>
-    var today = new Date();
-    var currentMonth = today.getMonth();
-    var currentYear = today.getFullYear();
-    var selectYear = document.getElementById("year");
-    var selectMonth = document.getElementById("month");
-    var monthHeader = document.getElementById("monthHeader");
-    var yearHeader = document.getElementById("yearHeader");
-    var nextBtn = document.getElementById("next");
-    var previousBtn = document.getElementById("previous");
-    var datePicked = document.getElementById("date-picked");
-    var months = "";
-    var days = "";
-    var monthsArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var daysArr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-    months = monthsArr;
-    days = daysArr;
-
-    var tableHeaderMonth = document.getElementById("thead-month");
-    var dataHead = "<tr>";
-    var startDay = "";
-
-    for (dhead in days) {
-      days[dhead] === "Sun" ? startDay = "red-text" : startDay = "";
-      dataHead += "<th data-days='" + days[dhead] + "' class='" + startDay + "'>" + days[dhead] + "</th>";
-    }
-
-    dataHead += "</tr>";
-    tableHeaderMonth.innerHTML = dataHead;
-    showCalendar(currentMonth, currentYear);
-
-    nextBtn.addEventListener("click", next, false);
-    previousBtn.addEventListener("click", previous, false);
-
-    function yearRange(start, end) {
-      var years = "";
-
-      for (var year = start; year <= end; year++) {
-        years += "<option value='" + year + "'>" + year + "</option>";
-      }
-
-      return years;
-    }
-
-    var createYear = yearRange(1970, 2050);
-    selectYear.innerHTML = createYear;
-
-    function next() {
-      currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
-      currentMonth = (currentMonth + 1) % 12;
-      showCalendar(currentMonth, currentYear);
-    }
-
-    function previous() {
-      currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
-      currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-      showCalendar(currentMonth, currentYear);
-    }
-
-    function jump() {
-      currentYear = parseInt(selectYear.value);
-      currentMonth = parseInt(selectMonth.value);
-      showCalendar(currentMonth, currentYear);
-    }
-
-    function showCalendar(month, year) {
-      var firstDay = new Date(year, month).getDay();
-      var monthString = monthsArr[month];
-
-      table = document.getElementById("calendar-body");
-      table.innerHTML = "";
-      monthHeader.innerHTML = monthString.substring(0, 3);
-      yearHeader.innerHTML = year;
-      selectYear.value = year;
-      selectMonth.value = month;
-
-      var date = 1;
-
-      for (var i = 0; i < 6; i++) {
-        var row = document.createElement("tr");
-
-        for (var j = 0; j < 7; j++) {
-          if (i === 0 && j < firstDay) {
-            cell = document.createElement("td");
-            cellText = document.createTextNode("");
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-          } else if (date > daysInMonth(month, year)) {
-            break;
-          } else {
-            cell = document.createElement("td");
-            cell.setAttribute("data-date", date);
-            cell.setAttribute("data-month", month + 1);
-            cell.setAttribute("data-year", year);
-            cell.setAttribute("data-month-name", months[month]);
-            cell.className = "date-picker";
-            cell.innerHTML = "<span>" + date + "</span>";
-            // Tambahkan validasi tanggal yang sudah lewat
-            var currentDate = new Date();
-            currentDate.setHours(0, 0, 0, 0); // Set jam ke 00:00:00
-
-            var cellDate = new Date(year, month, date);
-
-
-
-            // Modify the onclick function to include saving to the form input
-            cell.onclick = function(event) {
-              var dates = document.querySelectorAll('.date-picker');
-              var currentTarget = event.currentTarget;
-              var date = currentTarget.dataset.date;
-              var month = currentTarget.dataset.month - 1;
-              var year = currentTarget.dataset.year;
-
-              for (var i = 0; i < dates.length; i++) {
-                dates[i].classList.remove('selected');
-              }
-
-              currentTarget.classList.add('selected');
-              datePicked.innerHTML = date + ' ' + monthsArr[month] + ' ' + year;
-
-              // Save to the form input
-              document.getElementById('selectedDate').value = year + '-' + pad(month + 1, 2) + '-' + pad(date, 2);
-            };
-
-            function pad(number, length) {
-              var str = '' + number;
-              while (str.length < length) {
-                str = '0' + str;
-              }
-              return str;
-            }
-
-
-            if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-              cell.className = "date-picker selected";
-            }
-
-            if (cellDate < currentDate) {
-              cell.classList.add("disabled");
-              cell.onclick = null; // Hapus event onclick untuk tanggal yang sudah lewat
-            }
-
-            row.appendChild(cell);
-            date++;
-          }
-        }
-
-        table.appendChild(row);
-      }
-    }
-
-    function daysInMonth(month, year) {
-      return 32 - new Date(year, month, 32).getDate();
-    }
-  </script>
-  <!-- <script src="assets/js/date-picker.js"></script> -->
+  <script src="assets/js/date-picker.js"></script>
   <script src="./assets/js/vendors/jquery.min.js"></script>
   <script src="./assets/js/vendors/bootstrap.min.js"></script>
   <script src="./assets/js/vendors/enquire.min.js"></script>
