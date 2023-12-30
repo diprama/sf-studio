c3fda34bdcfe61ab5e3b4da417a93b63267b2ed9
diprama <50134297+diprama@users.noreply.github.com>
2023-12-30 10:27:32 +0700

    Update test_booking.php

--- booking/test_booking.php
+++ booking/test_booking.php
@@ -355,9 +355,11 @@ if (isset($_POST['btnSubmit'])) {
 
                           <div class="btn-area mt-10">
                             <button class="btn secondary btn-large block waves-effect" name="btnSubmit" type=" submit">Confirm Booking</button>
-                                  <br>
                           </div>
-                          <a class="btn primary btn-large block waves-effect" href="https://sf-selfstudio.com/booking/">Pilih Ulang Tanggal</a>
+                          <br>
+                          <div>
+                            <a class="btn primary btn-large block waves-effect" href="https://sf-selfstudio.com/booking/">Pilih Ulang Tanggal</a>    
+                          </div>
 
                           </form>
 
