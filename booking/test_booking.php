afd839c13c8c05db31682c55c56910f908a38ba0
diprama <50134297+diprama@users.noreply.github.com>
2023-12-30 11:21:05 +0700

    Update test_booking.php

--- booking/test_booking.php
+++ booking/test_booking.php
@@ -193,9 +193,6 @@ if (isset($_POST['btnSubmit'])) {
                         <div class="head">
                           <div class="title-main align-left">
                             <h4 class="secondary"><span>BOOKING</span></h4>
-                            <p class="desc use-text-subtitle2">Jam Buka : Senin 08:00 - 19:00</p>
-                            <p class="desc use-text-subtitle2">Jam Buka : Selasa 08:00 - 19:00</p>
-
                           </div>
                           <!-- <a class="btn btn-flat waves-effect button-link" href="register.html">
                             <i class="material-icons icon left mr-1">arrow_forward</i>Create new account
