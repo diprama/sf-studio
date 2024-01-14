98a0899788f150a51cbd082d5ec6a95bdc4d33a8
diprama <50134297+diprama@users.noreply.github.com>
2024-01-14 11:55:39 +0700

    Update master_jadwal_update_status.php

--- admin/master_jadwal_update_status.php
+++ admin/master_jadwal_update_status.php
@@ -18,8 +18,6 @@ include "library/inc.connection.php";
       SET `status`=0,`updated_date`=now() WHERE id='$id'";
     $myQry   = mysqli_query($koneksidb, $mySql)  or die("ERROR BOOKING:  " . mysqli_error($koneksidb));
     $nomor  = 0;
-
-
    # Validasi Insert Sukses
     if ($myQry) {
       echo "<meta http-equiv='refresh' content='0; url=?page=Master-Jadwal&status=ok'>";
