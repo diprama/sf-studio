ae9d104f438b322148e307ac73f93e2d559228b6
diprama <50134297+diprama@users.noreply.github.com>
2024-01-14 12:48:22 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -11,9 +11,9 @@
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>LOGIN - SF SelfStudio</title>
-    <!-- <link rel="apple-touch-icon" href="/app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="/app-assets/images/ico/favicon.ico">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet"> -->
+    <link rel="apple-touch-icon" href="../app-assets/images/logo/logo-sf.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/logo/logo-sf.png">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
 
     <!-- BEGIN: Vendor CSS-->
