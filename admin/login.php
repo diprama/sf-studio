da0eae206c1062915b29b01dc63fb6082cd2930c
diprama <50134297+diprama@users.noreply.github.com>
2024-01-14 12:56:27 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -23,7 +23,7 @@
     <!-- BEGIN: Theme CSS-->
     <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap-extended.css">
-    <!-- <link rel="stylesheet" type="text/css" href="../../../app-assets/css/colors.css"> -->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/colors.css">
     <link rel="stylesheet" type="text/css" href="../../../app-assets/css/components.css">
     <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/dark-layout.css">
     <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/bordered-layout.css">
